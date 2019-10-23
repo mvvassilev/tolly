7d0e7b5b1560bf6d6a226404a67a40dc25509f4a
mvvassilev <m.v.vassilev@abv.bg>
2019-10-23 16:09:02 +0800

    Filtering items by id works

--- items/views.py
+++ items/views.py
@@ -7,3 +7,8 @@ def index(request):
     items_list = Item.objects.all()
     context = { 'items_list' : items_list}
     return  render(request, 'items/index.html', context)
+
+#filter by item_code
+def filter(request, item_code):
+    item = Item.objects.filter(pk=item_code)
+    return render(request, 'items/{{ item.name }}.html', {'item': item})
